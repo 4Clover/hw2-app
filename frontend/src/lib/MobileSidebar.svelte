abc199cee2d2cc7276998aa0a0f141e68136194f
4Clover <dlmannion@ucdavis.edu>
2025-04-27 15:43:29 -0700

    started process of converting hw1-src -> Svelte framework model

---
+++ frontend/src/lib/MobileSidebar.svelte
@@ -0,0 +1,55 @@
+﻿<script lang="ts">
+    const menuToggle = document.getElementById('nav-toggle'); // hamburger menu (mobile)
+    const sidebar = document.getElementById('mobile-sidebar'); // sidebar
+    const closeBtn = document.getElementById('close-mobile-sidebar'); // sidebar close button
+    const overlay = document.getElementById('overlay'); // dimmer
+
+    if (menuToggle && sidebar && closeBtn && overlay) { // element existence check
+
+        const openSidebar = () => {
+            sidebar.classList.add('open'); // update state for sidebar
+            overlay.classList.add('active'); // update state for overlay
+            // Accessibility
+            menuToggle.setAttribute('aria-expanded', 'true'); // ARIA update
+            sidebar.setAttribute('aria-hidden', 'false'); // ARIA update
+        };
+
+        const closeSidebar = () => {
+            sidebar.classList.remove('open'); // update state for sidebar
+            overlay.classList.remove('active'); // update state for overlay
+            // Accessibility
+            menuToggle.setAttribute('aria-expanded', 'false'); // ARIA update
+            sidebar.setAttribute('aria-hidden', 'true'); // ARIA update
+        };
+
+        // event listeners
+        menuToggle.addEventListener('click', openSidebar); // listener for open sidebar event
+        // listeners for close sidebar event
+        closeBtn.addEventListener('click', closeSidebar);
+        overlay.addEventListener('click', closeSidebar);
+
+    } else {
+        // error message for missing elements
+        console.warn("Error: Sidebar functionality disabled check sidebar elements.");
+    }
+</script>
+
+
+<button class="close-mobile-sidebar" id="close-mobile-sidebar" aria-label="Close navigation menu">x</button>
+<ul>
+		<li><a href=" ">U.S.</a></li>
+		<li><a href=" ">World</a></li>
+		<li><a href=" ">Business</a></li>
+		<li><a href=" ">Arts</a></li>
+		<li><a href=" ">Lifestyle</a></li>
+		<li><a href=" ">Opinion</a></li>
+		<li class="mobile-divider"></li>
+		<li><a href=" ">Audio</a></li>
+		<li><a href=" ">Games</a></li>
+		<li><a href=" ">Cooking</a></li>
+		<li><a href=" ">Wirecutter</a></li>
+		<li><a href=" ">The Athletic</a></li>
+		<li class="mobile-divider"></li>
+		<li><a href=" ">Search</a></li>
+		<li><a href=" ">Log In</a></li>
+</ul>
