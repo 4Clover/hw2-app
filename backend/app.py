b6ae9af3c3fa95f03a994b9759f9d3f7dd4ca009
Alyssa Vallejo <92827768+AV-CompSci-Mage@users.noreply.github.com>
2025-04-29 20:03:35 -0700

    Small tweaks to the UI, sacramento centric!

--- backend/app.py
+++ backend/app.py
@@ -133,6 +133,8 @@ def fetch_nyt_articles():
 
     # TODO: Add {begin_date, end_date, page, sort} arguments
     search_query = request.args.get('query', default="davis")
+    search_begin = request.args.get('begin')
+    search_end = request.args.get('end')
     search_filter = request.args.get('filter') # None most of the time
     search_page = request.args.get('page', default=0, type=int)
     # debug info for reference, check vite.config.ts for what is actually being sent
@@ -142,9 +144,14 @@ def fetch_nyt_articles():
         'api-key': api_key,
         'page': search_page,
     }
+    if search_begin:
+        params['begin_date'] = search_begin # format year-month-day (no dashes!)
+    if search_end:
+        params['end_date'] = search_end # same thing here (ex: 20250401)
     if search_filter: # add fq as a param if it actually has something, the added '&' would break the API call otherwise
         params['fq'] = search_filter
-
+    
+    
     BASE_NYT_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
     # docs reference:
         # payload = {'key1': 'value1', 'key2': ['value2', 'value3']}
